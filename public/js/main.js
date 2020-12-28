6b94385feb6178a68c9e9c41539208628b2f50c7
DawiT <Dawid@Tararuj.be>
2020-12-28 12:30:28 +0100

    exo 3

---
+++ public/js/main.js
@@ -0,0 +1,47 @@
+let phrase = "J'aime les tartines et j'en mange ";
+
+console.log(phrase);
+
+let cmbjour = " 4 par jour "; 
+
+
+console.log(phrase+cmbjour);
+
+let ch5 = 5; 
+let ch35 = 35; 
+
+console.log(ch5+ch35); 
+
+console.log(phrase+ch5);
+
+
+// ------------ 2 ------------------- 
+
+let hello = "Salut t le monde ";
+
+console.log(hello); 
+
+let gbye = "aurevoir! "; 
+
+console.log(gbye); 
+
+hello = gbye; 
+
+console.log(hello); 
+
+// ------------- 3 ---------------- 
+
+let ch4 = 4; 
+let ch6 = "6"; 
+
+console.log(ch4 > ch6);
+
+console.log(ch4 < ch6);
+
+console.log(ch4 == ch6);
+
+console.log(ch4 != ch6);
+
+console.log(ch4 === ch6);
+
+console.log(ch4 !== ch6);
